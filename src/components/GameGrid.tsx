cdc9c4d27a680727aed96a4ca9b8429aceb60f19
Joy <joyyujiepeng@gmail.com>
2023-12-25 09:10:22 +0000

    feat: add card loading skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { GameQuery } from "../App"
 import useGames, { Game } from "../hooks/useGames"
 import { Box, SimpleGrid } from "@chakra-ui/react"
 import GameCard from "./GameCard"
+import GameCardSkeleton from "./GameCardSkeleton"
 
 type GameGridProps = {
   gameQuery: GameQuery
@@ -9,6 +10,20 @@ type GameGridProps = {
 
 const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { data: games, error, isLoading } = useGames(gameQuery)
+  if (isLoading) {
+    return (
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
+        {" "}
+        {Array.from({ length: 8 }).map((_, i) => {
+          return <GameCardSkeleton key={i} />
+        })}
+      </SimpleGrid>
+    )
+  }
 
   return (
     <SimpleGrid
