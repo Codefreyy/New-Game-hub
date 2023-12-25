c2c5e8372a0fce6b6f789a1204b3bb5c0c8d82c1
Joy <joyyujiepeng@gmail.com>
2023-12-25 09:44:33 +0000

    feat: render the genre list

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
           />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside">
+          <GridItem area="aside" paddingX={5}>
             <GenreList />
           </GridItem>
         </Show>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,59 @@
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react"
 import useGenres from "../hooks/useGenres"
+import getCroppedImageUrl from "../services/getCroppedImageUrl"
 
 const GenreList = () => {
-  const { data } = useGenres()
-  return <div>{JSON.stringify(data)}</div>
+  const { data: genres, isLoading } = useGenres()
+  if (isLoading)
+    return (
+      <>
+        <Skeleton height="750px">
+          <SkeletonText></SkeletonText>
+        </Skeleton>
+      </>
+    )
+  return (
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => {
+          return (
+            <ListItem paddingY="5px" paddingLeft="5px" key={genre.id}>
+              <HStack>
+                <Image
+                  objectFit="cover"
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Button
+                  fontWeight="normal"
+                  variant="link"
+                  fontSize="lg"
+                  whiteSpace="normal"
+                  textAlign="left"
+                >
+                  {" "}
+                  {genre.name}
+                </Button>
+              </HStack>
+            </ListItem>
+          )
+        })}
+      </List>
+    </>
+  )
 }
 
 export default GenreList
