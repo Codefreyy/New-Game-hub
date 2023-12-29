afccf5fa9f7ac9d25d25adacf3cb07a8eba9cda7
Joy <joyyujiepeng@gmail.com>
2023-12-29 12:09:30 +0000

    UI: improve skeleton size

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react"
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
-      <Skeleton height="200px"></Skeleton>
+    <Card width={"100%"} height={"100%"} borderRadius={10} overflow="hidden">
+      <Skeleton height={{ sm: "400px", md: "200px" }}></Skeleton>
       <CardBody>
         <SkeletonText />
       </CardBody>
